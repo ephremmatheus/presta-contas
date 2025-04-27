169c56ccfc4fa8b092ff36878031be206e56c7e6
ephremmatheus <ephrem08matheus@gmail.com>
2025-04-27 10:50:15 -0300

    navbar responsivo e pronto

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import './Nav.css';
+
+/*assim q carregar verifica tamanho e poe */
+export const Nav = () => {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+
+    function handleClick(){
+        setClick(!click);
+    }
+
+    function closeMobileMenu(){
+        setClick(false);
+    }
+
+    function showButton() {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else{
+            setButton(true);
+        }
+    }
+
+    window.onload = function(){
+        showButton()
+    }
+    
+    window.addEventListener('resize', showButton);
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <Link to="/">teste</Link>
+                </div>
+                <div className="menu-icon" onClick={handleClick}>
+                    <span className={"material-symbols-outlined"} >
+                        {click? "close": "menu"}
+                    </span>
+                </div>
+                <ul className={click? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links">Funcionalidades</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links">Demostração</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links">Contato</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links-mobile">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" onClick={closeMobileMenu} className="nav-links-mobile-cadastrar">Cadastre-se</Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>CADASTRE-SE</Button>}
+            </div>
+        </nav>
+    )
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/Button"
+import { Nav } from "../components/nav"
+
+export const Home = () => {
+    return(
+        <>
+            <Nav />
+        </>
+        
+        
+    )
+}
